f7a31c78d54a4e288e74a462c14a15c45af5cfa7
okokoooppp <okokoooppp@gmail.com>
2017-10-12 23:27:39 +0800

    123

---
+++ JY-BizEng/javascripts/index.js
@@ -0,0 +1,17 @@
+﻿/*
+// 以下兩種為建立有名字之ready函數方式
+//這裡不能亂排,會失效
+$(Mark);
+$(document).ready(Mark);
+*/
+$(function() { //若不建立函數名字,直接這樣就可
+    $("td").dblclick(function(){
+        var bc = $(this).css("background-color")
+        /*alert(bc);*/
+        if (bc == "rgba(0, 0, 0, 0)") //只能用alert裡顯示的rgba, 用#會沒用
+            $(this).css({backgroundColor: "yellow"}); //除非建立後台伺服器,才可以click後永久變
+        else
+            $(this).css({backgroundColor: "rgba(0, 0, 0, 0)"});
+    });
+    /*$("header a").css({color: "#000"}); //這樣才會永久改變css屬性 */    
+})
